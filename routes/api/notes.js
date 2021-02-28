bb5a600bb4e1fd2399d42503599324ea2ec05dad
rolanduwxcc <rolanduwxcc@gmail.com>
2021-02-28 16:53:31 -0600

    trying to get delete refresh

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 // const { fstat } = require('fs');
 const router = express.Router();
-const { notes } = require('../../db/db.json'); 
+const { notes: notesArray } = require('../../db/db.json'); 
 let { idLastUsed } = require('../../db/db.json');
 
 //----since moving route here and defining route in server
@@ -11,14 +11,14 @@ let { idLastUsed } = require('../../db/db.json');
 //----is defined in the server.js
 //----------------------------------------------GET ROUTES
 router.get('/',(req, res) => {
-    res.json(notes);
+    res.json(notesArray);
 });
 
 router.get('/:id', (req, res) => {
     // res.send(req.params.id);
-    const found = notes.some(note => note.id === parseInt(req.params.id));
+    const found = notesArray.some(note => note.id === parseInt(req.params.id));
     if (found) {
-        res.json(notes.filter(note => note.id === parseInt(req.params.id)));
+        res.json(notesArray.filter(note => note.id === parseInt(req.params.id)));
     } else {
         //bad request
         res.status(400).json({msg: `No member with the id of ${req.params.id}`});
@@ -29,6 +29,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     // res.send(req.body);
     // console.log(notes);
+    // console.log(idLastUsed);
     const newNote = {
         title: req.body.title,
         text: req.body.text,
@@ -39,25 +40,25 @@ router.post('/', (req, res) => {
         return res.status(400).json({ msg: `Please include BOTH a title and text!`});
     }
 
-    notes.push(newNote);
+    notesArray.push(newNote);
     console.log(`idLastUsed = ${idLastUsed} || and newNote.id = ${newNote.id}`);
     idLastUsed = newNote.id;
 
     fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
-        JSON.stringify({ notes: notes, idLastUsed: newNote.id}, null, 2)
+        JSON.stringify({ notes: notesArray, idLastUsed: newNote.id}, null, 2)
     );
 
-    res.json(notes);
+    res.json(notesArray);
 });
 
 //------------------------------------------UPDATE ROUTES
 router.put('/:id', (req, res) => {
-    const found = notes.some(note => note.id === parseInt(req.params.id));
+    const found = notesArray.some(note => note.id === parseInt(req.params.id));
 
     if (found) {
         const updatedNote = req.body;
-        notes.forEach(note => {
+        notesArray.forEach(note => {
             if (note.id === parseInt(req.params.id)) {
                 note.title = updatedNote.title ? updatedNote.title : note.title;
                 note.text = updatedNote.text ? updatedNote.text : note.text;
@@ -73,17 +74,23 @@ router.put('/:id', (req, res) => {
 
 //------------------------------------------DELETE ROUTES
 router.delete('/:id', (req, res) => {
-    const found = notes.some(note => note.id === parseInt(req.params.id));
+    const found = notesArray.some(note => note.id === parseInt(req.params.id));
     console.log('smack the route',found);
+    console.log(notesArray);
+    console.log(idLastUsed);
+
     if (found) {
-        res.json
+
+        let newNotesList = notesArray.filter(note => note.id !== parseInt(req.params.id));
+        // console.log(newNotesList);
+
         fs.writeFileSync(
             path.join(__dirname, '../../db/db.json'),
-            JSON.stringify({ notes: notes.filter(note => note.id !== parseInt(req.params.id)), idLastUsed: idLastUsed}, null, 2)
+            JSON.stringify({notes: newNotesList, idLastUsed: idLastUsed}, null, 2)
         );
-        console.table(notes);
-        // res.json(notes);
-        res.status(200).json({msg: `Note deleted!`});
+        console.table(newNotesList);
+        res.json(newNotesList);
+        // res.status(200).json({msg: `Note deleted!`});
         // res.json({ 
         //     msg: 'Note deleted', 
         //     notes: notes.filter(note => note.id !== parseInt(req.params.id))
